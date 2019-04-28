803b0826277d3a1e95dd55f2cf0b770ba58a3466
Ashley Murray <42747966+ashleyjmurray@users.noreply.github.com>
2019-04-28 00:39:24 -0400

    Update app.R

--- app.R
+++ app.R
@@ -1,131 +1,55 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-library(ggplot2)
-library(dplyr)
-library(tidyverse)
-library(broom)
-library(knitr)
-library(shiny)
-y15 <- read.csv("PERM_Disclosure_Data_FY15_Q4.csv", header = TRUE)
+# Load packages -----------------------------------------------------
 
-# Define UI for application that draws a histogram
+library(tidyverse)
+library(ggplot2)
+library(shiny)
+
+# Load data ---------------------------------------------------------
+# Replace with `read.csv` or `read_csv` (tidyverse)
+movies <- read.csv("workplease.csv", header = TRUE)
+str(movies) # CLEAN THE DATA (COMBINE LEVELS, MAKE VAR NUMERIC)
+movies = movies[1:200,]
+
+
+# Shiny App ---------------------------------------------------------
+
+
 ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel("hi guys"),
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
+  # Sidebar layout with a input and output definitions
+  sidebarLayout(
+    
+    # Inputs: Select variables to plot
+    sidebarPanel(
       
-   )
+      # Select variable for y-axis
+      selectInput(inputId = "y", 
+                  label = "Y-axis:",
+                  choices = c("DECISION_DATE", "CASE_RECEIVED_DATE", "EMPLOYER_NAME", 
+                              "EMPLOYER_STATE", "JOB_INFO_EDUCATION"), 
+                  selected = "DECISION_DATE"),
+      
+      # Select variable for x-axis
+      selectInput(inputId = "x", 
+                  label = "X-axis:",
+                  choices = "CASE_STATUS", 
+                  selected = "CASE_STATUS")
+    ),
+    
+    # Output: Show scatterplot
+    mainPanel(
+      plotOutput(outputId = "scatterplot")
+    )
+  )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-     y15 %>% distinct(CASE_STATUS)
-     y15 <- y15 %>%
-       mutate(CASE_STATUS = case_when(
-         CASE_STATUS == "DENIED" ~ "Denied",
-         CASE_STATUS == "CERTIFIED" ~ "Certified",
-         CASE_STATUS == "CERTIFIED-EXPIRED" ~ "Certified-Expired",
-         CASE_STATUS == "WITHDRAWN" ~ "Withdrawn",
-         TRUE ~ CASE_STATUS
-       ))
-     
-     # Add binary var for certified/not certified
-     y15 <- y15 %>%
-       filter(!is.na(CASE_STATUS)) %>%
-       mutate(CERT = case_when(
-         CASE_STATUS %in% c("Certified", "Certified-Expired") ~ "Certified",
-         CASE_STATUS == "Denied" ~ "Not Certified",
-         TRUE ~ NA_character_
-       ))
-     
-     y15_country <- y15 %>%
-       # Filter N/A Certs
-       filter(!is.na(CERT)) %>%
-       group_by(COUNTRY_OF_CITIZENSHIP) %>%
-       count(CERT) %>%
-       mutate(prop = n / sum(n)) %>%
-       # Don't count incomplete data 
-       filter(!is.na(COUNTRY_OF_CITIZENSHIP))
-     
-     # Compute overall average of accepted applicants
-     prop_accepted <- y15 %>%
-       count(CERT) %>%
-       mutate(prop_accepted = n / sum(n)) %>%
-       filter(CERT == "Certified") %>%
-       pull(prop_accepted)
-     
-     # Top 10 proportion of countries w/ > 500 applicants
-     y15_country %>%
-       filter(sum(n) > 500) %>%
-       filter(CERT == "Certified") %>%
-       arrange(desc(prop)) %>%
-       head(10) %>%
-       ggplot(aes(x = reorder(COUNTRY_OF_CITIZENSHIP, prop), 
-                  y = prop)) +
-       geom_bar(stat = "identity") +
-       geom_hline(aes(yintercept = prop_accepted,
-                      colour = "Average Prop"),
-                  linetype = "dashed", show.legend = TRUE) +
-       guides(colour = guide_legend(title = NULL)) +
-       coord_flip() +
-       labs(title = "Countries with Top 10 Acceptance Rate for Permanent Workers, 2008 - 2019",
-            subtitle = "For countries n > 500",
-            x = "Country",
-            y = "Proportion of Certified Applicants")
-     
-     # Bottom 10 proportion of countries w/ > 500 applicants
-     y15_country %>%
-       filter(sum(n) > 500) %>%
-       filter(CERT == "Certified") %>%
-       arrange(prop) %>%
-       head(10) %>%
-       ggplot(aes(x = reorder(COUNTRY_OF_CITIZENSHIP, -prop), 
-                  y = prop)) +
-       geom_bar(stat = "identity") +
-       geom_hline(aes(yintercept = prop_accepted,
-                      colour = "Average Prop"),
-                  linetype = "dashed", show.legend = TRUE) +
-       guides(colour = guide_legend(title = NULL)) +
-       coord_flip() +
-       labs(title = "Countries with Bottom 10 Acceptance Rate for Permanent Workers, 2008 - 2019",
-            subtitle = "For countries n > 500",
-            x = "Country",
-            y = "Proportion of Certified Applicants")
-     
-     y15_country %>%
-       filter(CERT == "Certified") %>%
-       ggplot(aes(x = prop)) +
-       geom_histogram() +
-       geom_vline(aes(fill = "Average Prop."),
-                  xintercept = prop_accepted,
-                  colour = "red",
-                  linetype = "dashed") +
-       labs(title = "Distribution of Permanent Worker Acceptance Rates",
-            subtitle = "All countries, 2008 - 2019",
-            x = "Proportion Accepted",
-            y = "Number of Countries")
-   }
-     
-   )
-     
+server <- function(input, output, session) {
+  
+  # Create scatterplot object the plotOutput function is expecting
+  observeEvent(input$y, {
+    output$scatterplot <- renderPlot({
+      ggplot(data = movies, aes_string(x = input$y, fill = input$x)) + geom_bar()
+    })
+  })
 }
 
-# Run the application 
 shinyApp(ui = ui, server = server)
-
